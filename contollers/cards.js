7e2715fa9632911f665f773a50cddfedb37d2f30
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-05-25 22:04:45 +0300

    feat: добавить роутеры и контроллеры лайка и дизлайка карточки

--- contollers/cards.js
+++ contollers/cards.js
@@ -1,14 +1,12 @@
 const Card = require('../models/card');
 
 module.exports.getCards = (req, res) => {
-  console.log('GET_CARDS');
   Card.find({})
     .then((cards) => res.status(200).send({ data: cards }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.deleteCardById = (req, res) => {
-  console.log('DELETE_CARD');
   const { cardId: _id } = req.params;
   Card.deleteOne({ _id })
     .then((card) => res.status(200).send({ data: card }))
@@ -16,7 +14,6 @@ module.exports.deleteCardById = (req, res) => {
 };
 
 module.exports.createCard = (req, res) => {
-  console.log('CREATE_CARD');
   const { name, link } = req.body;
   Card.create({
     name,
@@ -27,3 +24,19 @@ module.exports.createCard = (req, res) => {
     .then((card) => res.status(201).send({ data: card }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
+module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $addToSet: { likes: req.user._id } },
+  { new: true },
+)
+  .then((card) => res.status(200).send({ data: card }))
+  .catch((err) => res.status(500).send({ message: err.message }));
+
+module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $pull: { likes: req.user._id } },
+  { new: true },
+)
+  .then((card) => res.status(200).send({ data: card }))
+  .catch((err) => res.status(500).send({ message: err.message }));
--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const { getCards, createCard, deleteCardById } = require('../contollers/cards');
+const {
+  getCards,
+  createCard,
+  deleteCardById,
+  likeCard,
+  dislikeCard,
+} = require('../contollers/cards');
 
 router.get('/', getCards);
 router.post('/', createCard);
 router.delete('/:cardId', deleteCardById);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
 
 module.exports = router;
