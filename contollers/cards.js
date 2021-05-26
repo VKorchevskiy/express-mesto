6c639df506aa2f5399190e5e20168e711372dcaa
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-05-26 23:13:32 +0300

    feat: установить формат возвращаемых данных

--- contollers/cards.js
+++ contollers/cards.js
@@ -1,16 +1,34 @@
 const Card = require('../models/card');
 
+const convertCard = (card) => {
+  const collectedCard = {
+    likes: card.likes,
+    _id: card._id,
+    name: card.name,
+    link: card.link,
+    owner: card.owner,
+    createdAt: card.createdAt,
+  };
+  return collectedCard;
+};
+
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.status(200).send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((cards) => {
+      res.status(200).send(cards.map((card) => convertCard(card)));
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.deleteCardById = (req, res) => {
   const { cardId: _id } = req.params;
   Card.deleteOne({ _id })
     .then((card) => res.status(200).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.createCard = (req, res) => {
@@ -21,8 +39,10 @@ module.exports.createCard = (req, res) => {
     owner: req.user._id,
     createdAt: Date.now(),
   })
-    .then((card) => res.status(201).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => res.status(200).send(convertCard(card)))
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
@@ -30,13 +50,17 @@ module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   { $addToSet: { likes: req.user._id } },
   { new: true },
 )
-  .then((card) => res.status(200).send({ data: card }))
-  .catch((err) => res.status(500).send({ message: err.message }));
+  .then((card) => res.status(200).send(convertCard(card)))
+  .catch((err) => {
+    res.status(500).send({ message: err.message });
+  });
 
 module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } },
   { new: true },
 )
-  .then((card) => res.status(200).send({ data: card }))
-  .catch((err) => res.status(500).send({ message: err.message }));
+  .then((card) => res.status(200).send(convertCard(card)))
+  .catch((err) => {
+    res.status(500).send({ message: err.message });
+  });
--- contollers/users.js
+++ contollers/users.js
@@ -1,56 +1,56 @@
 const User = require('../models/user');
 
+const convertUser = (user) => {
+  const collectedUser = {
+    name: user.name,
+    about: user.about,
+    avatar: user.avatar,
+    _id: user._id,
+  };
+  return collectedUser;
+};
+
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => {
-      res.status(200).send({ data: users });
+      res.status(200).send(users.map((user) => convertUser(user)));
     })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.getUserById = (req, res) => {
   const { userId: _id } = req.params;
   User.findById({ _id })
-    .then((user) => res.status(200).send({
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      _id: user._id,
-    }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(200).send(convertUser(user)))
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.status(201).send({
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      _id: user._id,
-    }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(200).send(convertUser(user)))
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.patchInfo = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
-    .then((user) => res.status(200).send({
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      _id: user._id,
-    }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(200).send(convertUser(user)))
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.patchAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { avatar: req.body.avatar }, { new: true })
-    .then((user) => res.status(200).send({
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      _id: user._id,
-    }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(200).send(convertUser(user)))
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
