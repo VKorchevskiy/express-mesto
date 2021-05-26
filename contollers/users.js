3ca25109e853e379a381fda4573898845efba4b1
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-05-26 20:25:43 +0300

    add patchInfo and patchAvatar

--- contollers/users.js
+++ contollers/users.js
@@ -1,7 +1,6 @@
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
-  // console.log('GET_USERS');
   User.find({})
     .then((users) => {
       res.status(200).send({ data: users });
@@ -10,33 +9,48 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUserById = (req, res) => {
-  // console.log('GET_USER');
   const { userId: _id } = req.params;
   User.findById({ _id })
-    .then((users) => res.status(200).send({ data: users }))
+    .then((user) => res.status(200).send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      _id: user._id,
+    }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.createUser = (req, res) => {
-  // console.log('CREATE_USER');
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.status(201).send({ data: user }))
+    .then((user) => res.status(201).send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      _id: user._id,
+    }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.patchInfo = (req, res) => {
   const { name, about } = req.body;
-  // console.log('PATCH_INFO_USER');
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
-    .then((user) => res.status(200).send({ data: user }))
+    .then((user) => res.status(200).send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      _id: user._id,
+    }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.patchAvatar = (req, res) => {
-  const { avatar } = req.body;
-  // console.log('PATCH_AVATAR');
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-    .then((user) => res.status(200).send({ data: user }))
+  User.findByIdAndUpdate(req.user._id, { avatar: req.body.avatar }, { new: true })
+    .then((user) => res.status(200).send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      _id: user._id,
+    }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
