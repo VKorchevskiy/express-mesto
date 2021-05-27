767706655fae434b590e03c737de3a1f91efd073
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-05-27 21:42:44 +0300

    fix: исправить обработку ошибок

--- contollers/cards.js
+++ contollers/cards.js
@@ -13,35 +13,42 @@ const convertCard = (card) => {
 };
 
 module.exports.getCards = (req, res) => {
-  Card.find({})
+  Card
+    .find({})
     .then((cards) => {
       res.status(200).send(cards.map((card) => convertCard(card)));
     })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
-    });
+    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию.' }));
 };
 
 module.exports.deleteCardById = (req, res) => {
   const { cardId: _id } = req.params;
-  Card.deleteOne({ _id })
+  Card
+    .deleteOne({ _id })
     .then((card) => res.status(200).send({ data: card }))
     .catch((err) => {
-      res.status(500).send({ message: err.message });
+      if (err.name === 'CastError') {
+        return res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+      }
+      return res.status(500).send({ message: 'Ошибка по умолчанию.' });
     });
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  Card.create({
-    name,
-    link,
-    owner: req.user._id,
-    createdAt: Date.now(),
-  })
-    .then((card) => res.status(200).send(convertCard(card)))
+  Card
+    .create({
+      name,
+      link,
+      owner: req.user._id,
+      createdAt: Date.now(),
+    })
+    .then((card) => res.status(201).send(convertCard(card)))
     .catch((err) => {
-      res.status(500).send({ message: err.message });
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
+      }
+      return res.status(500).send({ message: 'Ошибка по умолчанию.' });
     });
 };
 
@@ -52,7 +59,10 @@ module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
 )
   .then((card) => res.status(200).send(convertCard(card)))
   .catch((err) => {
-    res.status(500).send({ message: err.message });
+    if (err.name === 'CastError') {
+      return res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка.' });
+    }
+    return res.status(500).send({ message: 'Ошибка по умолчанию.' });
   });
 
 module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
@@ -62,5 +72,8 @@ module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
 )
   .then((card) => res.status(200).send(convertCard(card)))
   .catch((err) => {
-    res.status(500).send({ message: err.message });
+    if (err.name === 'CastError') {
+      return res.status(400).send({ message: 'Переданы некорректные данные для снятия лайка.' });
+    }
+    return res.status(500).send({ message: 'Ошибка по умолчанию.' });
   });
