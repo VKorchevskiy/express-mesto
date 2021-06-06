6359c275a0d52529c0e47be4748f64c6817d6155
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-06-06 22:22:15 +0300

    добавить мидлвар авторизиции и защитить роуты авторизацией

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const { login, createUser } = require('../controllers/users');
+const { auth } = require('../middlewares/auth');
 
 router.post('/signin', login);
 router.post('/signup', createUser);
+
+router.use(auth);
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
