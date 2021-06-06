c22891401a7fdf7d8ec3e7508e85ae3b8673e096
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-06-06 22:19:39 +0300

    удалить захардкоженого пользователя

--- app.js
+++ app.js
@@ -15,14 +15,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '60abe203b09fb93e8cb555ed', // вставьте сюда _id созданного в предыдущем пункте пользователя
-  };
-
-  next();
-});
-
 app.use(routes);
 
 app.listen(PORT, () => {
