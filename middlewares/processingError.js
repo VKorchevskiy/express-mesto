c8192a845c3d7b0755b58734faa49fcbb3284b31
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-06-10 22:31:20 +0300

    Сделать единую обработку ошибок

--- middlewares/processingError.js
+++ middlewares/processingError.js
@@ -1,13 +1,5 @@
 module.exports.processingError = (err, req, res, next) => {
-  let { statusCode = 500, message } = err;
-  if (err.name === 'CastError' || err.name === 'ValidationError') {
-    statusCode = 400;
-    message = 'Переданы некорректные данные.';
-  }
-  if (err.name === 'MongoError' && err.code === 11000) {
-    statusCode = 409;
-    message = 'Пользователь с такой почтой уже есть.';
-  }
+  const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message: statusCode === 500
       ? 'На сервере произошла ошибка.'
